76f76ed45a8615b0b84f64b676ec5d9a5f6dc853
sbhardw8 <sandeep_bhardwaj@optum.com>
2019-04-02 14:35:21 +0530

    Change in timeout

--- src/main/java/com/koko/concurrency/future/FutureExample.java
+++ src/main/java/com/koko/concurrency/future/FutureExample.java
@@ -10,36 +10,36 @@ import java.util.concurrent.TimeoutException;
 
 public class FutureExample {
 
-	public static void main(String[] args) {
-		ExecutorService service = Executors.newSingleThreadExecutor();
-		SumTask sumTask = new FutureExample().new SumTask(20);
-		Future<Integer> future = service.submit(sumTask);
-		Integer result = 0;
-		try {
-			result = future.get(2000, TimeUnit.MILLISECONDS);
-		} catch (InterruptedException | ExecutionException | TimeoutException e) {
-			e.printStackTrace();
-		}
-		System.out.println(result);
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newSingleThreadExecutor();
 
-	}
+        SumTask sumTask = new SumTask(20);
+        Future<Integer> future = service.submit(sumTask);
+        Integer result = 0;
+        try {
+            result = future.get(3, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+        }
+        System.out.println(result);
 
-	class SumTask implements Callable<Integer> {
-		private int num = 0;
+    }
+}
 
-		public SumTask(int num) {
-			this.num = num;
-		}
+class SumTask implements Callable<Integer> {
+    private int num;
 
-		@Override
-		public Integer call() throws Exception {
-			int result = 0;
-			for (int i = 1; i <= num; i++) {
-				result += i;
-				Thread.sleep(1000);
-			}
-			return result;
-		}
-	}
+    public SumTask(int num) {
+        this.num = num;
+    }
 
+    @Override
+    public Integer call() throws Exception {
+        int result = 0;
+        for (int i = 1; i <= num; i++) {
+            result += i;
+            Thread.sleep(100);
+        }
+        return result;
+    }
 }
