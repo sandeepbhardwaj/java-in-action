b083da6bf939c4ad10d7c1a8add14802c21784ea
sbhardw8 <sandeep_bhardwaj@optum.com>
2019-04-02 17:19:52 +0530

    Custom thread pool

--- src/main/java/com/koko/concurrency/threadpool/ThreadPool.java
+++ src/main/java/com/koko/concurrency/threadpool/ThreadPool.java
@@ -5,7 +5,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class ThreadPool {
     private LinkedBlockingQueue<Runnable> queue;
 
-
     public ThreadPool(int queueSize, int nThreads) {
         queue = new LinkedBlockingQueue<>(queueSize);
 
@@ -20,6 +19,34 @@ public class ThreadPool {
             queue.put(task);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * WorkerThread class to take the tasks from queue and perform the task
+     */
+    private class WorkerThread extends Thread {
+        private LinkedBlockingQueue<Runnable> queue;
+
+        public WorkerThread(String name, LinkedBlockingQueue<Runnable> queue) {
+            super(name);
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    String name = Thread.currentThread().getName();
+                    Runnable task = queue.take();
+                    System.out.println("Task Started by Thread :" + name);
+                    task.run();
+                    System.out.println("Task Finished by Thread :" + name);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
