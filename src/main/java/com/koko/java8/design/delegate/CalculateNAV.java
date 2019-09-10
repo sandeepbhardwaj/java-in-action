9bf263ae37901a2c9f3d2def466456fd49405e2a
sbhardw8 <sandeep_bhardwaj@optum.com>
2019-09-10 15:01:56 +0530

    Delegate design pattern using java8

--- src/main/java/com/koko/java8/design/delegate/CalculateNAV.java
+++ src/main/java/com/koko/java8/design/delegate/CalculateNAV.java
@@ -6,9 +6,23 @@ import java.util.function.Function;
 public class CalculateNAV {
 	private Function<String, BigDecimal> priceFinder;
 
-	public BigDecimal computeStockWorth(
-			final String ticker, final int shares) {
+	public CalculateNAV(final Function<String, BigDecimal> aPriceFinder) {
+		priceFinder = aPriceFinder;
+	}
+
+
+	public BigDecimal computeStockWorth(final String ticker, final int shares) {
 		return priceFinder.apply(ticker).multiply(BigDecimal.valueOf(shares));
+	}
+
+
+	public static void main(String[] args) {
+		/**
+		 * Passing the method reference
+		 */
+		final CalculateNAV calculateNav = new CalculateNAV(YahooFinance::getPrice);
+		System.out.println(String.format("100 shares of Google worth: $%.2f",
+				calculateNav.computeStockWorth("GOOGLE", 100)));
 	}
 
 }
