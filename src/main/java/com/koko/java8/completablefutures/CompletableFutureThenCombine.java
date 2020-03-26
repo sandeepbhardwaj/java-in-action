151ff0a699472cf8c55862a3466ff5b668b98a16
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2020-03-26 17:15:55 +0530

    gradle to maven

--- src/main/java/com/koko/java8/completablefutures/CompletableFutureThenCombine.java
+++ src/main/java/com/koko/java8/completablefutures/CompletableFutureThenCombine.java
@@ -11,24 +11,24 @@ import java.util.concurrent.CompletableFuture;
  */
 public class CompletableFutureThenCombine {
 
-	private static CompletableFuture<Integer> create(int data) {
-		return CompletableFuture.supplyAsync(() -> data);
-	}
+    private static CompletableFuture<Integer> create(int data) {
+        return CompletableFuture.supplyAsync(() -> data);
+    }
 
-	private static CompletableFuture<Integer> increment(int data) {
-		return CompletableFuture.supplyAsync(() -> data + 1);
-	}
+    private static CompletableFuture<Integer> increment(int data) {
+        return CompletableFuture.supplyAsync(() -> data + 1);
+    }
 
-	public static void main(String[] args) {
-		// thenCompose is used if you have an asynchronous mapping function (i.e. one
-		// that returns a CompletableFuture). It will then return a future with the
-		// result directly, rather than a nested future.
+    public static void main(String[] args) {
+        // thenCompose is used if you have an asynchronous mapping function (i.e. one
+        // that returns a CompletableFuture). It will then return a future with the
+        // result directly, rather than a nested future.
 
-		CompletableFuture.supplyAsync(() -> 1).thenCompose(x -> CompletableFuture.supplyAsync(() -> x + 1))
-				.thenAccept(data -> System.out.println(data));
+        CompletableFuture.supplyAsync(() -> 1).thenCompose(x -> CompletableFuture.supplyAsync(() -> x + 1))
+                .thenAccept(data -> System.out.println(data));
 
-		create(2)
-				// .thenApply(data -> increment(data))
-				.thenCompose(data -> increment(data)).thenAccept(data -> System.out.println(data));
-	}
+        create(2)
+                // .thenApply(data -> increment(data))
+                .thenCompose(data -> increment(data)).thenAccept(data -> System.out.println(data));
+    }
 }
--- src/main/java/com/koko/java8/completablefutures/CompletableFutureThenCompose.java
+++ src/main/java/com/koko/java8/completablefutures/CompletableFutureThenCompose.java
@@ -10,43 +10,6 @@ import java.util.concurrent.*;
  */
 public class CompletableFutureThenCompose {
 
-	private static int compute(int data) {
-		System.out.println(Thread.currentThread());
-		sleep(2);
-
-		return data;
-	}
-
-	private static CompletableFuture<Integer> create(int data, Executor executor) {
-		return CompletableFuture.supplyAsync(() -> compute(data), executor);
-	}
-
-	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-
-		Executor executor = Executors.newFixedThreadPool(Math.min(10, 100), new ThreadFactory() {
-
-			@Override
-			public Thread newThread(Runnable r) {
-				Thread t = new Thread(r);
-				t.setDaemon(true);
-				return t;
-			}
-		});
-
-		// thenCombine - is executed with the two results as arguments to the supplied
-		// function
-		CompletableFuture<Integer> future = create(2, executor)
-				.thenCombineAsync(create(3, executor), (result1, result2) -> result1 + result2)
-				.thenCombineAsync(create(5, executor), (result3, result4) -> result3 + result4)
-				.thenCombineAsync(create(10, executor), (result3, result4) -> result3 + result4);
-
-		System.out.println("Result :" + future.join());
-
-		long duration = (System.currentTimeMillis() - start) / 1_000;
-		System.out.println("Done in " + duration + " secs");
-	}
-
 	private static void sleep(int timeout) {
 		try {
 			TimeUnit.SECONDS.sleep(timeout);
@@ -54,4 +17,42 @@ public class CompletableFutureThenCompose {
 			e.printStackTrace();
 		}
 	}
+
+    private static int compute(int data) {
+        System.out.println(Thread.currentThread());
+        sleep(2);
+        return data;
+    }
+
+    private static CompletableFuture<Integer> create(int data, Executor executor) {
+        return CompletableFuture.supplyAsync(() -> compute(data), executor);
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        Executor executor = Executors.newFixedThreadPool(Math.min(10, 100), new ThreadFactory() {
+
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                return t;
+            }
+        });
+
+        // thenCombine - is executed with the two results as arguments to the supplied
+        // function
+        CompletableFuture<Integer> future = create(2, executor)
+                .thenCombineAsync(create(3, executor), (result1, result2) -> result1 + result2)
+                .thenCombineAsync(create(5, executor), (result3, result4) -> result3 + result4)
+                .thenCombineAsync(create(10, executor), (result3, result4) -> result3 + result4);
+
+        System.out.println("Result :" + future.join());
+
+        long duration = (System.currentTimeMillis() - start) / 1_000;
+        System.out.println("Done in " + duration + " secs");
+    }
+
+
 }
