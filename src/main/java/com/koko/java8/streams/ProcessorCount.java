7592283eb03e532b15d9a9574a987ee04739ff78
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2020-04-19 21:48:40 +0530

    Refactor

--- src/main/java/com/koko/java8/streams/ProcessorCount.java
+++
@@ -1,8 +0,0 @@
-package com.koko.java8.streams;
-
-public class ProcessorCount {
-
-	public static void main(String[] args) {
-		System.out.println("No of processors :" + Runtime.getRuntime().availableProcessors());
-	}
-}
