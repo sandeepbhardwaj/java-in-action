b5549585a8bb0cd88d45613d73ccd584d8c68136
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2022-06-06 14:27:58 +0530

    Update MessageService.java

--- src/main/java/com/koko/MessageService.java
+++ src/main/java/com/koko/MessageService.java
@@ -1,9 +1,20 @@
 package com.koko;
 
+import java.util.*;
+
 public class MessageService {
 
     public static String get() {
         return "Hello JUnit 5";
+    }
+
+    public static void main(String[] args) {
+
+        String x = "abcdef";
+        x = x.substring(1, 3);
+        System.out.println(x);
+
+
     }
 
 }
